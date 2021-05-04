93bb6613e2ce96ad506ab73c017350d836ba5ee5
AshHarDav2 <83212583+AshHarDav2@users.noreply.github.com>
2021-05-04 15:46:51 -0500

    Add files via upload

--- com/company/Day6/Task15.java
+++ com/company/Day6/Task15.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class Task15 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int length;
-        do {
-            length = scanner.nextInt();
-        } while (length < 3);
-        int i = 0, max;
-        while (i < length) {
-            int Number = scanner.nextInt();
-            max = Number;
-            if (Number>max){
+        int Number = scanner.nextInt();
+        int  max = Number, max2=0;
+        while (Number!=0) {
+            Number = scanner.nextInt();
+            if(Number==0)
+                break;
+            if (Number > max) {
+                max2 = max;
                 max = Number;
             }
-            System.out.println(max);
+            else if (Number>max2 && Number!=max)
+                max2 = Number;
         }
-
+        System.out.println(max2);
     }
 }
